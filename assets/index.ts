91dbc171bad3aec3bf49fc919291c038911f3b76
an.le6 <truongan1730@gmail.com>
2022-12-22 00:16:54 +0700

    1. chore: add logo assets

---
+++ assets/index.ts
@@ -0,0 +1,8 @@
+import LogoMajorBg from "./logo_major_bg.png";
+import LogoMajorText from "./logo_major_text.png";
+import LogoBlackBg from "./logo_black_bg.png";
+import LogoBlackText from "./logo_black_text.png";
+import LogoWhiteBg from "./logo_white_bg.png";
+import LogoWhiteText from "./logo_white_text.png";
+
+export { LogoMajorBg, LogoMajorText, LogoBlackBg, LogoBlackText, LogoWhiteBg, LogoWhiteText };
