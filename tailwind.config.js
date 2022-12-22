9d7752ea9b476e01b9db2edae681dee00e9e58e3
an.le6 <truongan1730@gmail.com>
2022-12-22 11:57:49 +0700

    4. chore: responsive libs and styles, global style adjust

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  darkMode: "class",
+  darkMode: "media", // TODO: adjust to 'class' when do toggler
   theme: {
     extend: {},
   },
