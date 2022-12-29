410f8ac17d05dc44fc203ffb6f436ccc04b9040b
an.le6 <truongan1730@gmail.com>
2022-12-29 20:26:25 +0700

    24. minor: add color constants

---
+++ consts/index.ts
@@ -0,0 +1,28 @@
+// CAN BE USED TO READ ONLY
+export const ColorScheme = {
+  purple: {
+    hex: "#7c3aed",
+    rgb: "rgb(124,58,237)",
+    accent: "rgba(124,58,237, 0.8)",
+    tw: "violet-600",
+  },
+  amber: {
+    hex: "#fbbf24",
+    rgb: "rgb(251, 191, 56)",
+    accent: "rgb(251, 191, 56, 0.8)",
+    tw: "amber-400",
+  },
+  blue: {
+    dark: {
+      hex: "#0d1626",
+      rgb: "rgb(13,22,38)",
+    },
+  },
+  pink: {
+    light: { hex: "#ee3c77", rgb: "rgb(238,60,119)" },
+    dark: {
+      hex: "#ec2c6c",
+      rgb: "rgb(236,44,108)",
+    },
+  },
+};
