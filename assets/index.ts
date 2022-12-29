8523bdbd17dd33a966a2eb20681280df5df32e13
an.le6 <truongan1730@gmail.com>
2022-12-29 20:25:27 +0700

    22: chore: clean assets

--- assets/index.ts
+++ assets/index.ts
@@ -1,21 +1,15 @@
 // images
 import LogoMajorBg from "./images/logo_major_bg.png";
-import LogoMajorText from "./images/logo_major_text.png";
 import LogoBlackBg from "./images/logo_black_bg.png";
-import LogoBlackText from "./images/logo_black_text.png";
 import LogoWhiteBg from "./images/logo_white_bg.png";
-import LogoWhiteText from "./images/logo_white_text.png";
 import SampleDashboardBanner from "./images/sample_dashboard_banner.png";
 import CelebHorizontal from "./images/celeb_horizontal.jpg";
 import CelebVertical from "./images/celeb_vertical.jpg";
 
 const images = {
   LogoMajorBg,
-  LogoMajorText,
   LogoBlackBg,
-  LogoBlackText,
   LogoWhiteBg,
-  LogoWhiteText,
   SampleDashboardBanner,
   CelebHorizontal,
   CelebVertical,
